2985cc8f0aabf11f720f6359b6cfb56ada7f9719
michal7272 <michal7272@seznam.cz>
2023-04-22 08:46:27 +0200

    Update app.js
    Close side menu when a link is clicked

--- app.js
+++ app.js
@@ -2,14 +2,24 @@
 const menuIcon = document.getElementById("menu-icon");
 const closeIcon = document.getElementById("close-icon");
 const ul = document.querySelector("ul");
-const body = document.querySelector("body");
+const navLinks = document.querySelectorAll(".side-menu a");
 
+// Open side menu when the menu-icon is clicked
 menuIcon.addEventListener("click", () => {
     ul.classList.toggle("show");
     closeIcon.classList.toggle("show");
 });
 
+// Close side menu when the close-icon is clicked
 closeIcon.addEventListener("click", () => {
     ul.classList.toggle("show");
     closeIcon.classList.toggle("show");
+});
+
+// Close side menu when a link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        ul.classList.toggle("show");
+        closeIcon.classList.toggle("show");
+    })
 });
